6ef9d98dfaec413e6284865b785a595c235c2df0
Opoks <opokuj883@gmail.com>
2025-06-26 12:31:25 +0000

    changed object

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,10 +1,10 @@
 const data=[
     {
-        question:'What school did i attend?',
-        options:['GTUC','Legon','UPSA','UCC'],
+        question:'What is the name of the longest river in the world?',
+        options:['Congo river','Mississippi river','Amazon river','Nile river'],
         score:0,
         clicked:'',
-        ans:'GTUC',
+        ans:'Nile river',
         id:1
     },
     {
@@ -16,28 +16,68 @@ const data=[
        id:2
     },
     {
-        question:'What is a female dog called?',
-        options:['she dog','bitch','dog','calf'],
+        question:'What year did World War II end?',
+        options:[1945,1921,1954,1960],
         score:0,
         clicked:'',
-        ans:'bitch',
+        ans:1945,
        id:3
     },
     {
-        question:'Who is the owner of Tesla ?',
-        options:['Nana Addo','Trump','Elon Musk','Mr Beast'],
+        question:'What is the name of the planet closest to the sun ?',
+        options:['Jupiter','Mercury','Mars','Earth'],
         score:0,
         clicked:'',
-        ans:'Elon Musk',
+        ans:'Mercury',
         id:4
     },
     {
-        question:'which continent is lions mostly found?',
-        options:['Africa','Asia','Europe','South America'],
+        question:'what is the name of the smallest country in the world?',
+        options:['Togo','Vatican city','Ghana','Turkey'],
         score:0,
         clicked:'',
-        ans:'Africa',
+        ans:'Vatican city',
         id:5
+    },
+    {
+        question:'what is the highest mountain in the world?',
+        options:['Afajato','Kilimanjaro','Everest','Makalu'],
+        score:0,
+        clicked:'',
+        ans:'Everest',
+        id:6
+    },
+    {
+        question:'which of these animals is a nocturnal?',
+        options:['Cow','Goat','Owl','Duck'],
+        score:0,
+        clicked:'',
+        ans:'Owl',
+        id:7
+    },
+    {
+        question:'Which of these is not a source protein?',
+        options:['Chicken','Beans','Rice','Milk'],
+        score:0,
+        clicked:'',
+        ans:'Rice',
+        id:8
+    },
+    {
+        question:'which of the following is not a sense organ?',
+        options:['Tongue','Skin','Liver','Nose'],
+        score:0,
+        clicked:'',
+        ans:'Liver',
+        id:9
+    },
+    {
+        question:'what is the name of a female lion?',
+        options:['Lioness','Huntress','She-lion','Tigress'],
+        score:0,
+        clicked:'',
+        ans:'Lioness',
+        id:10
     }
 ]
 export default data;
